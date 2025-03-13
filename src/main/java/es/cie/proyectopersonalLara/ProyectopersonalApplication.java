2bbfc1f3fd745b45844209baf2e7a6872152a55d
cie <celebrin_gil@hotmail.com>
2025-03-13 10:01:27 +0100

    inicio

---
+++ src/main/java/es/cie/proyectopersonalLara/ProyectopersonalApplication.java
@@ -0,0 +1,13 @@
+package es.cie.proyectopersonalLara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectopersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectopersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cie/proyectopersonalLara/ProyectopersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cie.proyectopersonalLara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
